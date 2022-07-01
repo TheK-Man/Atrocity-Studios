1ff3b49eac251ec5862063ffce5a09c58da7f388
TheK-Man <k-yannacopoulos@hotmail.com>
2022-06-30 22:10:24 -0700

    ENEMY ai/SHOOTING

--- My project/Assets/Enemy.cs
+++ My project/Assets/Enemy.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header ("Stats")]
+    public float speed;
+    public float stoppingDistance;
+    public float nearDistance;
+    public float startTimeBtwShots;
+    private float timeBtwShots;
+
+    [Header ("References")]
+    public GameObject shot;
+    private Transform player;
+
     public int health = 100;
 
     public GameObject deathEffect;
@@ -17,7 +28,31 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
+    void Update ()
+    {
+        //Makes the enemy move
+        if(Vector2.Distance(transform.position, player.position) < nearDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        } else if(Vector2.Distance(transform.position, player.position) > stoppingDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > nearDistance){
+            transform.position = this.transform.position;
+        }
+        
+        //Makes the enemy shoot
+        if(timeBtwShots <= 0){
+            Instantiate(shot, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        } else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+    }
 
     void Die ()
     {
